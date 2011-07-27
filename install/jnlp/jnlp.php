3e21dc5c911ea12d121bd0187e2dd3e4c8b9ac30
Codeko <codeko@codeko.com>
2011-07-27 20:34:59 +0200

    La versión ahora se recupera del archivo version.txt en vez de estar en el mismo php

--- install/jnlp/jnlp.php
+++ install/jnlp/jnlp.php
@@ -5,15 +5,16 @@ header("Cache-Control: no-store, no-cache, must-revalidate");
 header("Cache-Control: post-check=0, pre-check=0", false);
 // HTTP/1.0 
 header("Pragma: no-cache");
+$version = file_get_contents("version.txt");
 //Propiedades de configuración de la aplicación
 $properties = array();
 echo '<?xml version="1.0" encoding="UTF-8"?>';
-$codebase = "http://" . $_SERVER['HTTP_HOST'] . dirname($_SERVER['REQUEST_URI'])."/";
+$codebase = "http://" . $_SERVER['HTTP_HOST'] . dirname($_SERVER['REQUEST_URI']) . "/";
 ?>
 <jnlp
     spec="6.0+"
     codebase="<?= $codebase ?>"
-    version="2.10.RC1"
+    version="<?= $version ?>"
     href="jnlp.php">
     <information>
         <title>Maimónides</title>
@@ -35,11 +36,11 @@ $codebase = "http://" . $_SERVER['HTTP_HOST'] . dirname($_SERVER['REQUEST_URI'])
     <update check="always"/>
     <resources>
         <j2se version="1.6+" java-vm-args="-Xmx512m"/>
-        <jar href="Maimonides.jar" size="<?=filesize("Maimonides.jar")?>" main="true"/>
+        <jar href="Maimonides.jar" size="<?= filesize("Maimonides.jar") ?>" main="true"/>
         <?php
         foreach (scandir("lib") As $name) {
             if (strlen($name) > 4 && substr($name, -4) == ".jar") {
-                echo "<jar size=\"".filesize("lib/".$name)."\" href=\"lib/" . $name . "\"/>\n";
+                echo "<jar size=\"" . filesize("lib/" . $name) . "\" href=\"lib/" . $name . "\"/>\n";
             }
         }
         ?>
